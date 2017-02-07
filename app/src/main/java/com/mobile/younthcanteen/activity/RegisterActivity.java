9c7da0b1872ff7406bff1975dba9950cad96d9a5
huangjian <huangjian1@hydsoft.com>
2017-02-07 23:13:33 +0800

    登录逻辑基本完成

---
+++ app/src/main/java/com/mobile/younthcanteen/activity/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.mobile.younthcanteen.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * author：hj
+ * time: 2017/2/7 0007 23:12
+ */
+
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
