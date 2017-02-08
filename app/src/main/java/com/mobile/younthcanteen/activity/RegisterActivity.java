46379b5af6ef99500a54fdd3dfd914c481de80a9
huangjian <huangjian1@hydsoft.com>
2017-02-08 10:39:06 +0800

    注册界面完成

--- app/src/main/java/com/mobile/younthcanteen/activity/RegisterActivity.java
+++ app/src/main/java/com/mobile/younthcanteen/activity/RegisterActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.mobile.younthcanteen.R;
 
@@ -13,10 +15,19 @@ import com.mobile.younthcanteen.R;
  */
 
 public class RegisterActivity extends Activity {
+
+    private TextView tvStep1;
+    private TextView tvStep2;
+    private TextView tvStep3;
+    private LinearLayout llStep1;
+    private LinearLayout llStep2;
+    private LinearLayout llStep3;
     private EditText etPhone;
-    private EditText etPassword;
-    private EditText etRePassword;
     private EditText etCode;
+    private EditText etPwd;
+    private EditText etRePwd;
+    private Button btnSendCode;
+    private Button btnCommitCode;
     private Button btnGetCode;
     private Button btnRegister;
 
@@ -29,10 +40,21 @@ public class RegisterActivity extends Activity {
     }
 
     private void initView() {
+        tvStep1 = (TextView) findViewById(R.id.tv_step1);
+        tvStep2 = (TextView) findViewById(R.id.tv_step2);
+        tvStep3 = (TextView) findViewById(R.id.tv_step3);
+
+        llStep1 = (LinearLayout) findViewById(R.id.ll_step1);
+        llStep2 = (LinearLayout) findViewById(R.id.ll_step2);
+        llStep3 = (LinearLayout) findViewById(R.id.ll_step3);
+
         etPhone = (EditText) findViewById(R.id.et_phone);
-        etPassword  = (EditText) findViewById(R.id.et_password);
-        etRePassword  = (EditText) findViewById(R.id.et_repassword);
-        etCode  = (EditText) findViewById(R.id.et_code);
+        etCode = (EditText) findViewById(R.id.et_code);
+        etPwd = (EditText) findViewById(R.id.et_pwd);
+        etRePwd = (EditText) findViewById(R.id.et_repwd);
+
+        btnSendCode = (Button) findViewById(R.id.btn_sendcode);
+        btnCommitCode = (Button) findViewById(R.id.btn_commitcode);
         btnGetCode = (Button) findViewById(R.id.btn_getcode);
         btnRegister = (Button) findViewById(R.id.btn_register);
     }
