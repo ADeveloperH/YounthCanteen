20d5eac91c8727e66ad575a2d015b9905b003820
huangjian <huangjian1@hydsoft.com>
2017-02-08 09:16:13 +0800

    注册界面完成

--- app/src/main/java/com/mobile/younthcanteen/activity/RegisterActivity.java
+++ app/src/main/java/com/mobile/younthcanteen/activity/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.mobile.younthcanteen.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mobile.younthcanteen.R;
 
@@ -11,9 +13,27 @@ import com.mobile.younthcanteen.R;
  */
 
 public class RegisterActivity extends Activity {
+    private EditText etPhone;
+    private EditText etPassword;
+    private EditText etRePassword;
+    private EditText etCode;
+    private Button btnGetCode;
+    private Button btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_layout);
+
+        initView();
+    }
+
+    private void initView() {
+        etPhone = (EditText) findViewById(R.id.et_phone);
+        etPassword  = (EditText) findViewById(R.id.et_password);
+        etRePassword  = (EditText) findViewById(R.id.et_repassword);
+        etCode  = (EditText) findViewById(R.id.et_code);
+        btnGetCode = (Button) findViewById(R.id.btn_getcode);
+        btnRegister = (Button) findViewById(R.id.btn_register);
     }
 }
