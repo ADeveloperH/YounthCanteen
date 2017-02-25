42b6fe81389146336687eaccb0f0a5d6b6ab06de
huangjian <huangjian1@hydsoft.com>
2017-02-25 11:46:27 +0800

    首页轮播图显示效果完成

---
+++ app/src/main/java/com/mobile/younthcanteen/adapter/HomeFragmentPagerAdapter.java
@@ -0,0 +1,84 @@
+package com.mobile.younthcanteen.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+
+import com.mobile.younthcanteen.bean.HomeDataBean;
+
+import java.util.List;
+
+public class HomeFragmentPagerAdapter extends PagerAdapter {
+
+	private List<HomeDataBean.TopEntity> viewPagerDataList;
+	private Context context;
+	private List<ImageView> imageList;
+	public HomeFragmentPagerAdapter(Context context, List<HomeDataBean.TopEntity> viewPagerDataList, List<ImageView> imageList) {
+		this.context = context;
+		this.viewPagerDataList = viewPagerDataList;
+		this.imageList = imageList;
+	}
+
+	@Override
+	public int getCount() {
+		return imageList == null ? 0 :imageList.size();
+	}
+
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return view == object;
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+//		container.removeView((View)object);
+		if(position < imageList.size()){
+			((ViewPager)container).removeView(imageList.get(position));
+		}
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		// 第一件事：根据position 获得一个对应的view ，并将view，添加至 container
+		View view = imageList.get(position);
+		view.setOnClickListener(new MyOnClickListener((Integer) view.getTag()));
+		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		container.addView(view, params);
+		// 第二件事：返回一个和view 有关系 的对象。
+		return view;
+	}
+
+	private class MyOnClickListener implements OnClickListener{
+		private int curPosition = 0;
+		public MyOnClickListener(int position) {
+			curPosition = position;
+		}
+
+		@Override
+		public void onClick(View v) {
+//			HomePageViewPagerItemBean homePageViewPagerItemBean = viewPagerDataList.get(curPosition);
+//			if(homePageViewPagerItemBean != null){
+//				if(!LoginUtil.checkLoginState(act)){
+//					return;
+//				}
+//				String className = homePageViewPagerItemBean.getClass_name_android();
+//				String linkUrl = homePageViewPagerItemBean.getImage_link();
+//				String moduleName = homePageViewPagerItemBean.getContent();
+//				MobclickAgent.onEvent(act,"home_banner");
+//				FunctionModularUtil.handleClickEvent(act,className,linkUrl,moduleName);
+//			}
+		}
+
+	}
+
+	public void setDataList(List<HomeDataBean.TopEntity> viewPagerDataList,
+							List<ImageView> imageList){
+		this.viewPagerDataList = viewPagerDataList;
+		this.imageList = imageList;
+	}
+}
--- app/src/main/java/com/mobile/younthcanteen/fragment/HomeFragment.java
+++ app/src/main/java/com/mobile/younthcanteen/fragment/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.mobile.younthcanteen.fragment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -12,20 +15,28 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.mobile.younthcanteen.R;
+import com.mobile.younthcanteen.adapter.HomeFragmentPagerAdapter;
 import com.mobile.younthcanteen.adapter.HomeListAdapter;
 import com.mobile.younthcanteen.bean.HomeDataBean;
 import com.mobile.younthcanteen.http.Http;
 import com.mobile.younthcanteen.http.MyTextAsyncResponseHandler;
 import com.mobile.younthcanteen.http.RequestParams;
+import com.mobile.younthcanteen.util.BitmapUtil;
+import com.mobile.younthcanteen.util.FileUtil;
 import com.mobile.younthcanteen.util.JsonUtil;
 import com.mobile.younthcanteen.util.ToastUtils;
+import com.mobile.younthcanteen.util.UIUtils;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * author：hj
  * time: 2017/2/7 0007 15:15
  */
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements ViewPager.OnPageChangeListener {
     private View rootView;//缓存Fragment的View
     private boolean isNeedReLoad = true;//是否需要重新加载该Fragment数据
     private ListView lvHome;
@@ -33,6 +44,56 @@ public class HomeFragment extends Fragment {
     private ImageView ivBanner;
     private ViewPager viewPager;
     private LinearLayout llPointGroup;
+    private final static int VIEWPAGEDATAGETSUCCESS = 0;
+    private final static int VIEWPAGEDATAGETFAILURE = 1;
+    private final static int SHOWVIEWPAGER = 2;
+    private final static int CHANGVIEWPAGERITEM = 3;
+    private List<ImageView> imageList;// 轮播图图片的集合
+    private List<HomeDataBean.TopEntity> viewPagerDataList;// 轮播图的数据集合
+    private MyHandler mHandler = new MyHandler(this);
+
+    private static class MyHandler extends Handler {
+        private WeakReference<HomeFragment> homeFragmentWeakReference;
+
+        public MyHandler(HomeFragment homeFragment) {
+            homeFragmentWeakReference = new WeakReference<HomeFragment>(
+                    homeFragment);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            final HomeFragment homeFragment = homeFragmentWeakReference.get();
+            if (homeFragment == null) {
+                return;
+            }
+            switch (msg.what) {
+                case VIEWPAGEDATAGETSUCCESS:// 轮播图请求成功
+                    break;
+                case SHOWVIEWPAGER:// 展示轮播图
+                    homeFragment.showViewPager();
+                    break;
+//                case VIEWPAGEDATAGETFAILURE:// 轮播图请求失败
+//                    if (!homeFragment.isGetViewPagerSus) {
+//                        homeFragment.viewPagerShowDefault();
+//                    }
+//                    break;
+                case CHANGVIEWPAGERITEM:// 轮播图自动切换
+                    // viewPager 切换至下一页
+                    if (homeFragment.viewPager != null) {
+                        homeFragment.viewPager
+                                .setCurrentItem(homeFragment.viewPager
+                                        .getCurrentItem() + 1);
+                        // 发送延时消息，发送后， 3000 毫秒后，执行handleMessage方法
+                        sendEmptyMessageDelayed(CHANGVIEWPAGERITEM, 6000);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,7 +117,8 @@ public class HomeFragment extends Fragment {
             mActivity = getActivity();
             initView(getView());
 //            initData();
-//            setListener();
+            setListener();
+
             getData();
             isNeedReLoad = false;
         }
@@ -64,14 +126,26 @@ public class HomeFragment extends Fragment {
 
     private void initView(View view) {
         lvHome = (ListView) view.findViewById(R.id.lv_home);
+        addLvHeaderView();
+        ivBanner = (ImageView) view.findViewById(R.id.iv_banner);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        llPointGroup = (LinearLayout) view.findViewById(R.id.ll_point_group);
+        ivBanner.setImageResource(R.drawable.shoucang_icon);
+    }
+
+    /**
+     * 为listview 添加头
+     */
+    private void addLvHeaderView() {
         LayoutInflater lif = (LayoutInflater) mActivity.
                 getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View headerView = lif.inflate(R.layout.fragment_home_lv_header,
                 lvHome, false);
         lvHome.addHeaderView(headerView);
-        ivBanner = (ImageView) view.findViewById(R.id.iv_banner);
-        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
-        llPointGroup = (LinearLayout) view.findViewById(R.id.ll_point_group);
+    }
+
+    private void setListener() {
+        viewPager.setOnPageChangeListener(this);
 
     }
 
@@ -85,8 +159,10 @@ public class HomeFragment extends Fragment {
                 if (null != homeDataBean) {
                     if (Http.SUCCESS.equals(homeDataBean.getReturnCode())) {
                         //成功
-                        HomeListAdapter adapter = new HomeListAdapter(mActivity, homeDataBean.getCenter());
-                        lvHome.setAdapter(adapter);
+                        showListView(homeDataBean.getCenter());
+                        viewPagerDataList = homeDataBean.getTop();
+                        initViewPagerData();
+
                     } else {
                         ToastUtils.showShortToast(homeDataBean.getReturnMessage());
                     }
@@ -102,5 +178,196 @@ public class HomeFragment extends Fragment {
                 ToastUtils.showShortToast("服务器异常,请稍后重试.");
             }
         });
+    }
+
+    /**
+     * 处理轮播图数据
+     */
+    private void initViewPagerData() {
+        if (viewPagerDataList == null || viewPagerDataList.size() == 0) {
+            //没有轮播图.显示默认图
+            viewPager.setVisibility(View.GONE);
+            llPointGroup.setVisibility(View.GONE);
+            ivBanner.setVisibility(View.VISIBLE);
+            return;
+        }
+        if (imageList == null) {
+            imageList = new ArrayList<ImageView>();
+        } else {
+            imageList.clear();
+        }
+        BitmapUtil bitmapUtil = new BitmapUtil(mActivity,
+                FileUtil.getCachePath(mActivity, "/bitmapcache"));
+        if (isAdded()) {
+            bitmapUtil.setLoadFailedDrawable(UIUtils.getDrawable(
+                    R.color.gray_bg));
+            bitmapUtil.setLoadingDrawable(UIUtils.getDrawable(
+                    R.color.gray_bg));
+        }
+
+        if (viewPagerDataList.size() >= 2) {
+            int location = viewPagerDataList.size() - 1;
+            final ImageView imageFirst = new ImageView(mActivity);
+            imageFirst.setTag(location);// 实际的位置
+            imageFirst.setImageResource(R.color.gray_bg);
+            imageFirst.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageList.add(imageFirst);
+            for (int i = 0; i < viewPagerDataList.size(); i++) {
+                final ImageView image = new ImageView(mActivity);
+                image.setTag(i);// 实际的位置
+                image.setScaleType(ImageView.ScaleType.FIT_XY);
+                image.setImageResource(R.color.gray_bg);
+                if (i == location) {
+                    bitmapUtil.display(image, viewPagerDataList.get(i)
+                            .getImg(), new BitmapUtil.BitmapLoadCallBack() {
+                        @Override
+                        public void onLoadCompleted(Bitmap bitmap) {
+                            image.setImageBitmap(bitmap);
+                            imageFirst.setImageBitmap(bitmap);
+                        }
+                    });
+                } else {
+                    if (i != 0) {
+                        bitmapUtil.display(image, viewPagerDataList.get(i).getImg());
+                    }
+                }
+                imageList.add(image);
+            }
+            final ImageView imageLast = new ImageView(mActivity);
+            imageLast.setTag(0);// 实际的位置
+            imageLast.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageLast.setImageResource(R.color.gray_bg);
+            bitmapUtil.display(imageLast, viewPagerDataList.get(0)
+                    .getImg(), new BitmapUtil.BitmapLoadCallBack() {
+                @Override
+                public void onLoadCompleted(Bitmap bitmap) {
+                    imageLast.setImageBitmap(bitmap);
+                    if (imageList.size() > 1) {
+                        imageList.get(1).setImageBitmap(bitmap);
+                    }
+                }
+            });
+            imageList.add(imageLast);
+        } else {
+            for (int i = 0; i < viewPagerDataList.size(); i++) {
+                ImageView image = new ImageView(mActivity);
+                image.setTag(i);// 实际的位置
+                image.setScaleType(ImageView.ScaleType.FIT_XY);
+                bitmapUtil.display(image, viewPagerDataList.get(i).getImg());
+                imageList.add(image);
+            }
+        }
+        mHandler.removeMessages(CHANGVIEWPAGERITEM);
+        mHandler.sendEmptyMessage(SHOWVIEWPAGER);
+    }
+
+
+    /**
+     * 显示轮播图
+     */
+    private void showViewPager() {
+        ivBanner.setVisibility(View.GONE);
+        viewPager.setVisibility(View.VISIBLE);
+        llPointGroup.setVisibility(View.VISIBLE);
+        llPointGroup.removeAllViews();
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                -2, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftMargin = 20;
+        if (viewPagerDataList.size() >= 2) {
+            for (int i = 0; i < viewPagerDataList.size(); i++) {
+                ImageView point = new ImageView(mActivity);
+                point.setBackgroundResource(R.drawable.point_bg);
+                llPointGroup.addView(point, layoutParams);
+                if (i == 0) {
+                    point.setEnabled(true);
+                } else {
+                    point.setEnabled(false);
+                }
+            }
+        }
+        mHandler.removeMessages(CHANGVIEWPAGERITEM);
+        if (viewPagerAdapter == null) {
+            viewPagerAdapter = new HomeFragmentPagerAdapter(
+                    mActivity, viewPagerDataList, imageList);
+            viewPager.setAdapter(viewPagerAdapter);
+        } else {
+            viewPagerAdapter.setDataList(viewPagerDataList, imageList);
+            viewPagerAdapter.notifyDataSetChanged();
+        }
+        if (viewPagerDataList.size() >= 2) {
+            // 自动循环
+            lastPosition = 1;
+            viewPager.setCurrentItem(1, true);// 显示第一个图片。
+            mHandler.sendEmptyMessageDelayed(CHANGVIEWPAGERITEM, 6000);
+        } else {
+            lastPosition = 0;
+            viewPager.setCurrentItem(0, true);// 显示第一个图片。
+        }
+    }
+
+
+
+    /**
+     * 显示套餐内容数据
+     *
+     * @param homeDataBean
+     */
+    private void showListView(List<HomeDataBean.CenterEntity> homeDataBean) {
+        HomeListAdapter adapter = new HomeListAdapter(mActivity, homeDataBean);
+        lvHome.setAdapter(adapter);
+    }
+
+    // 上一个选中的页面的下标
+    private int lastPosition = 0;
+    private HomeFragmentPagerAdapter viewPagerAdapter;
+    @Override
+    public void onPageScrollStateChanged(int state) {
+        if (state == ViewPager.SCROLL_STATE_IDLE) {
+            int curPosition = viewPager.getCurrentItem();
+            if (curPosition == imageList.size() - 1) {
+                viewPager.setCurrentItem(1, false);// 为了无限循环
+            } else if (curPosition == 0) {
+                viewPager.setCurrentItem(imageList.size() - 2, false);
+            }
+            viewPagerAdapter.notifyDataSetChanged();
+        }
+    }
+    @Override
+    public void onPageSelected(int position) {
+        // 切换指示点的选中状态
+        View lastChild = llPointGroup
+                .getChildAt(getRealIndexFromDataList(lastPosition));
+        View curChild = llPointGroup
+                .getChildAt(getRealIndexFromDataList(position));
+        if (lastChild != null) {
+            // 让上一个选中的点，显示为普通色
+            lastChild.setEnabled(false);
+        }
+        if (curChild != null) {
+            // 让当前选中的点显示为白色
+            curChild.setEnabled(true);
+        }
+        // 为lastPosition 设值
+        lastPosition = position;
+    }
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    /**
+     * 根据当前index获取实际在数据集合中的位置下标
+     *
+     * @param index 在图片集合imageList中的位置
+     * @return
+     */
+    private int getRealIndexFromDataList(int index) {
+        int realPosition = index - 1;
+        int count = viewPagerDataList.size();
+        if (realPosition < 0) {
+            realPosition += count;
+        } else if (realPosition >= count) {
+            realPosition -= count;
+        }
+        return realPosition;
     }
 }
